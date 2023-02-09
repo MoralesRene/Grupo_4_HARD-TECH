9e76fabb9ef7292f78d9b7197f1298c33544bb81
MoralesRene <renemoralesquispe@gmail.com>
2023-02-09 20:53:33 -0300

    home page and other created

--- app.js
+++ app.js
@@ -6,15 +6,38 @@ const path = require("path")
 const publicPath= path.join(__dirname,"public");
 
 app.use(express.static(publicPath))
-/** Para usar en las pruebas 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/home.html"))
-})
- */
 
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/index.html"))
+})
+app.get("/product-detail",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/product-detail.html"))
+})
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+app.get("/product-list",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/product-list.html"))
+})
+app.get("/product-cart",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/product-cart.html"))
+})
+app.get("/cart-ship",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/cart-ship.html"))
+})
+app.get("/cart-pay",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/cart-pay.html"))
+})
+app.get("/cart-resume",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/cart-resume.html"))
+})
 const port=3030;
 app.listen(port, ()=>{
     console.log("Server Started on http://localhost:"+port)
 })
+
 
 
