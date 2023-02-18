41888ccffe194ecd44db1ccd9ce1a7e1b5b6a5d4
Tizi04 <tiziluca@hotmail.com>
2023-02-17 19:11:39 -0500

    Creacion de ejs

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const register = require('../src/routes/register')
 const product = require('../src/routes/products')
 const cart = require('../src/routes/cart')
 const app = express();
-const publicPath= path.join(__dirname,"./public");
+const publicPath = path.join(__dirname, "./public");
 
 //Utilización de rutas
 app.use(express.static(publicPath))
@@ -16,9 +16,12 @@ app.use('/register', register)
 app.use('/product', product)
 app.use('/cart', cart)
 
+//Motor de plantilla
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+
 //Definicion de puertos y apertura de servidor
-const port= 3030;
-app.listen(port, ()=>{
+const port = 3030;
+app.listen(port, () => {
     console.log("Server Started on http://localhost:" + port)
 })
-
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 let homeController = {
     index: function (req, res) {
-        res.sendFile(path.join(__dirname,"../views/index.html"))
+        res.render("index.ejs")
     }
 }
 
