1d5a9234ca242be5b0d68f57912ca22cbcda8c26
MoralesRene <renemoralesquispe@gmail.com>
2023-02-23 15:13:28 -0300

    creacion product_list y data

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,16 +1,19 @@
 const path = require('path')
+const fs = require("fs")
 
+let ListJSON= fs.readFileSync(__dirname+"../../data/listProducts.json","utf-8")
+let listProducts = JSON.parse(ListJSON)
 let productsController = {
     index: function (req, res) {
         switch (req.params.element) {
             case 'detail':
-                res.render("product-detail")
+                res.render("product-detail",{productos:listProducts})
                 break;
             case 'list':
-                res.render("product-list")
+                res.render("product-list",{productos:listProducts})
                 break;
             case 'cart':
-                res.render("product-cart")
+                res.render("product-cart",{productos:listProducts})
                 break;
         }
     }
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const productsController = require('../controllers/productsController')
 
-router.get('/:element', productsController.index)
+router.get('/:element/', productsController.index)
 
 module.exports = router
