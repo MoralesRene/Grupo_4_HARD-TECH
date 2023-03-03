c548486db49d6230a84be8bb0fb62c9413a5e165
MoralesRene <renemoralesquispe@gmail.com>
2023-03-02 22:32:57 -0300

    Refactory productController

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,37 +1,39 @@
-const path = require('path')
-const fs = require("fs")
+const path = require('path');
+const fs = require("fs");
 
-let ListJSON= fs.readFileSync(__dirname+"../../data/listProducts.json","utf-8")
-let listProducts = JSON.parse(ListJSON)
+const productsFilePath = path.join(__dirname, "../data/listProducts.json")
+function getProducts() {
+    let ListJSON = fs.readFileSync(productsFilePath, "utf-8");
+    let listProducts = JSON.parse(ListJSON);
+    return listProducts;
+}
 
 let productsController = {
     index: function (req, res) {
+        const products = getProducts();
         switch (req.params.element) {
             case 'list':
-                res.render("product-list",{productos:listProducts})
+                res.render("product-list", { productos: products })
                 break;
             case 'cart':
-                res.render("product-cart",{productos:listProducts})
+                res.render("product-cart", { productos: products })
                 break;
             case 'edit':
                 res.render("edicion-producto")
         };
     },
-    mostrarPorCat: (req,res)=>{
+    mostrarPorCat: (req, res) => {
+        const products = getProducts();
+        const productByCategory = products.filter(producto => producto.category == req.params.category)
         if (req.params.category) {
-            res.render("product-list",{productos:listProducts.filter(producto=>{
-                return producto.category==req.params.category;
-            })})
+            res.render("product-list", { productos: productByCategory })
         }
     },
-    detalleID:(req,res)=>{
-       let idProd=listProducts.find(product=>{
-            return product.id==req.params.id;
-        })
-        let otherProd= listProducts.filter(product=>{
-            return product.category==idProd.category && product.id !==idProd.id;
-        })
-            res.render("product-detail",{idProd,otherProd}) 
+    detalleID: (req, res) => {
+        const products = getProducts();
+        let idProd = products.find(product => product.id == req.params.id)
+        let otherProd = products.filter(product => product.category == idProd.category && product.id !== idProd.id)
+        res.render("product-detail", { idProd, otherProd })
     }
 
 }
