f51308862dbf91dfd76f50e26fc332175cb81c77
FedericoGiovannacci <fgiovannacci@itba.edu.ar>
2023-04-25 21:44:14 -0300

    migraciones arregladas

--- src/database/migrations/20230425125447-create_products_table.js
+++ src/database/migrations/20230425125447-create_products_table.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -9,69 +9,65 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.createTable('products', {
+    await queryInterface.createTable("products", {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        primaryKey: true
+        primaryKey: true,
       },
       name: {
         type: Sequelize.TEXT,
-        allowNull: false
+        allowNull: false,
       },
       shortname: {
         type: Sequelize.TEXT,
-        allowNull: false
+        allowNull: false,
       },
       price: {
         type: Sequelize.DECIMAL(11, 2),
-        allowNull: false
+        allowNull: false,
       },
       model: {
         type: Sequelize.TEXT,
-        allowNull: false
+        allowNull: false,
       },
       product_categories_id: {
         type: Sequelize.INTEGER,
         references: {
-          model: "product-categories",
-          key: "id"
+          model: "product_categories",
+          key: "id",
         },
-        status_id: {
-          type: Sequelize.INTEGER,
-          references: {
-            model: "status",
-            key: "id"
-          },
-          trademarks_id: {
-            type: Sequelize.INTEGER,
-            references: {
-              model: "trademarks",
-              key: "id"
-            },
-            families_id: {
-              type: Sequelize.INTEGER,
-              references: {
-                model: "families",
-                key: "id"
-              },
-              warranties_id: {
-                type: Sequelize.INTEGER,
-                references: {
-                  model: "warranties",
-                  key: "id"
-                },
-
-              }
-
-            }
-
-          }
-        }
-      }
+      },
+      status_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "status",
+          key: "id",
+        },
+      },
+      trademarks_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "trademarks",
+          key: "id",
+        },
+      },
+      families_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "families",
+          key: "id",
+        },
+      },
+      warranties_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "warranties",
+          key: "id",
+        },
+      },
     });
   },
-
 
   async down(queryInterface, Sequelize) {
     /**
@@ -80,6 +76,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.dropTable('products');
-  }
+    await queryInterface.dropTable("products");
+  },
 };
