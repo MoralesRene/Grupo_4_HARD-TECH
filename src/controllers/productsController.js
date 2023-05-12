c685f4ba47a68ea0284db21fb623d15e53e52061
matiashuber <matiihy300@gmail.com>
2023-05-12 16:13:04 -0300

    Modificacion CRUD PRODUCTOS

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,69 +1,128 @@
-const path = require('path');
+const path = require("path");
 const fs = require("fs");
+const db = require("../database/models");
 
-const productsFilePath = path.join(__dirname, "../data/listProducts.json")
+// const productsFilePath = path.join(__dirname, "../data/listProducts.json");
 
-function getProducts() {
-    let ListJSON = fs.readFileSync(productsFilePath, "utf-8");
-    let listProducts = JSON.parse(ListJSON);
-    return listProducts;
-}
+// function getProducts() {
+//     let ListJSON = fs.readFileSync(productsFilePath, "utf-8");
+//     let listProducts = JSON.parse(ListJSON);
+//     return listProducts;
+// }
 
 let productsController = {
-    index: function (req, res) {
-        const products = getProducts();
-        switch (req.params.element) {
-            case 'list':
-                res.render("product-list", { productos: products })
-                break;
-            case 'cart':
-                res.render("product-cart", { productos: products })
-                break;
-                case 'create':
-                res.render("crear-producto")
-                break;
-        };
-    },
-    mostrarPorCat: (req, res) => {
-        const products = getProducts();
-        const productByCategory = products.filter(producto => producto.category == req.params.category)
-        if (req.params.category) {
-            res.render("product-list", { productos: productByCategory })
-        }
-    },
-    detalleID: (req, res) => {
-        const products = getProducts();
-        let idProd = products.find(product => product.id == req.params.id)
-        let otherProd = products.filter(product => product.category == idProd.category && product.id !== idProd.id)
-        res.render("product-detail", { idProd, otherProd })
-    },
-    editarProductoForm: (req, res) => {
-        const products = getProducts();
-        const product = products.find(element => element.id == req.params.id)
-        res.render('edicion-producto', { product })
-    },
-    editarProducto: (req, res) => {
-        const products = getProducts();
-        const productIndex = products.findIndex(element => element.id == req.params.id)
-        let imagenes = req.files.length !=0 ? req.files.map((element) => element.filename) : products[productIndex].image
-        products[productIndex] = {
-            ...products[productIndex],
-            name: req.body.name,
-            description: req.body.descripcion,
-            category: req.body.categoria,
-            trademark: req.body.marca,
-            price: req.body.price,
-            model: req.body.model,
-            image: imagenes
-        }
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2))
-        res.redirect('/')
-    },
-    eliminarProducto: (req, res) => {
-        const products = getProducts();
-        const productsFiltered = products.filter(element => element.id != req.params.id)
-        fs.writeFileSync(productsFilePath, JSON.stringify(productsFiltered, null, 2))
-        res.redirect('/')
+  index: async function (req, res) {
+    try {
+      const products = await db.Products.findAll();
+      switch (req.params.element) {
+        case "list":
+          res.render("product-list", { productos: products });
+          break;
+        case "cart":
+          res.render("product-cart", { productos: products });
+          break;
+        case "create":
+          res.render("crear-producto");
+          break;
+      }
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+  create: async (req, res) => {
+    try {
+      const productoCreado = await db.Products.create({
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.categoria,
+        trademark: req.body.marca,
+        price: req.body.price,
+        model: req.body.model,
+      });
+      res.redirect("/product/detail/" + productoCreado.id);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  mostrarPorCat: async (req, res) => {
+    try {
+      const products = await db.Products.findAll({
+        where: {
+          category: { name: req.params.category },
+        },
+        include: { model: db.category },
+      });
+
+      //   const productByCategory = products.filter(
+      //     (producto) => producto.category == req.params.category
+      //   );
+      //   if (req.params.category) {
+      //     res.render("product-list", { productos: productByCategory });
+      //   }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  detalleID: async (req, res) => {
+    try {
+      const products = await db.Products.findAll();
+      let idProd = req.params.id;
+      let otherProd = products.filter(
+        (product) =>
+          product.category == idProd.category && product.id !== idProd.id
+      );
+      res.render("product-detail", { idProd, otherProd });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  editarProductoForm: async (req, res) => {
+    try {
+      const product = await db.Products.findByPk(req.params.id);
+      res.render("edicion-producto", { product });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  editarProducto: async (req, res) => {
+    await db.Products.update(
+      {
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.categoria,
+        trademark: req.body.marca,
+        price: req.body.price,
+        model: req.body.model,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    const productIndex = products.findIndex(
+      (element) => element.id == req.params.id
+    );
+    let imagenes =
+      req.files.length != 0
+        ? req.files.map((element) => element.filename)
+        : products[productIndex].image;
+    products[productIndex] = {
+      ...products[productIndex],
+    };
+    res.redirect("/");
+  },
+  eliminarProducto: (req, res) => {
+    try {
+      db.Products.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 module.exports = productsController;
