269eb48d85f89b95b1eb9b4e7cdded3825bc027f
MoralesRene <renemoralesquispe@gmail.com>
2023-05-31 14:23:02 -0300

    product_detail view fixed

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,40 +4,50 @@ const db = require("../database/models")
 const { Op } = require("sequelize")
 
 let homeController = {
-    index: async (req, res)=> {
+    index: async (req, res) => {
         const productsFeatured = await db.Products.findAll({
-            where:{
+            where: {
                 status_id: 1
             }
         })
         const productsOffers = await db.Products.findAll({
-            where:{
+            where: {
                 status_id: 2
             }
         })
         const images = await db.Product_Images.findAll({
-            where:{
-              is_primary: true
+            where: {
+                is_primary: true
             }
-          })
-        res.render("index" ,{featuredProd:productsFeatured,offerProd:productsOffers,images})
+        })
+        const trademark = [
+            "marca-1.png",
+            "marca-2.png",
+            "marca-3.png",
+            "marca-4.png",
+            "marca-5.png",
+            "marca-6.png",
+            "marca-7.png",
+            "marca-8.png"
+        ]
+        res.render("index", { featuredProd: productsFeatured, offerProd: productsOffers, images,trademark })
     },
-    search: async (req,res)=> {
+    search: async (req, res) => {
         const products = await db.Products.findAll({
-            where:{
+            where: {
                 name: {
                     [Op.like]: `%${req.query.search}%`
                 }
             }
         })
         const images = await db.Product_Images.findAll({
-            where:{
-              is_primary: true
+            where: {
+                is_primary: true
             }
-          })
-          const trademarks = await db.Trademarks.findAll()
+        })
+        const trademarks = await db.Trademarks.findAll()
 
-        res.render("product-list",{productos:products,images,trademarks})
+        res.render("product-list", { productos: products, images, trademarks })
     }
 }
 
